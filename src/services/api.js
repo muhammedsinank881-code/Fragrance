43e77319eceb63c27513b156226b1e6e6e68da29
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-06-16 17:30:54 +0530

    updated version

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log(import.meta.env, "env");
 console.log(import.meta.env.VITE_API_URL, "url");
 export default API; 
