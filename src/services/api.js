9396c1d21cb735d1948513e60408f196decdbc8f
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-06-16 17:21:57 +0530

    updated version

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dilkacenter-eqagg4ame5dqdfae.canadacentral-01.azurewebsites.net",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     accept: "*/*",
     "Content-Type": "application/json",
   },
 });
-
+console.log(import.meta.env.VITE_API_URL, "url");
 export default API; 
